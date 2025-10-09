ba5b4424c2bd991f3cbed9134879c1156f98d461
szumaster3 <szumaster2009@protonmail.com>
2025-10-09 23:38:44 +0200

    Refactor HFTD quest using varbits

--- Server/src/main/content/region/fremennik/lighthouse/quest/horror/plugin/HorrorFromTheDeepPlugin.kt
+++ Server/src/main/content/region/fremennik/lighthouse/quest/horror/plugin/HorrorFromTheDeepPlugin.kt
@@ -1,13 +1,11 @@
 package content.region.fremennik.lighthouse.quest.horror.plugin
 
-import content.data.GameAttributes
 import content.region.fremennik.lighthouse.quest.horror.dialogue.JossikLighthouseDialogue
 import content.region.fremennik.lighthouse.quest.horror.dialogue.StrangeWallDialogue
 import core.api.*
+import core.api.getVarbit
 import core.api.openDoor
-import core.api.getQuestStage
-import core.api.isQuestComplete
-import core.api.setQuestStage
+import core.api.setVarbit
 import core.game.dialogue.DialogueFile
 import core.game.dialogue.FaceAnim
 import core.game.global.action.DoorActionHandler
@@ -26,9 +24,18 @@ class HorrorFromTheDeepPlugin : InteractionListener {
     private val brokenBridge = intArrayOf(Scenery.BROKEN_BRIDGE_4615, Scenery.BROKEN_BRIDGE_4616)
     private val strangeWalls = intArrayOf(Scenery.STRANGE_WALL_4544, Scenery.STRANGE_WALL_4543)
     private val strangeDoors = intArrayOf(Scenery.STRANGE_WALL_4545, Scenery.STRANGE_WALL_4546)
-    private val requiredItems = intArrayOf(Items.BRONZE_ARROW_882, Items.BRONZE_SWORD_1277, Items.AIR_RUNE_556, Items.FIRE_RUNE_554, Items.EARTH_RUNE_557, Items.WATER_RUNE_555)
+    private val requiredItems =
+        intArrayOf(
+            Items.BRONZE_ARROW_882,
+            Items.BRONZE_SWORD_1277,
+            Items.AIR_RUNE_556,
+            Items.FIRE_RUNE_554,
+            Items.EARTH_RUNE_557,
+            Items.WATER_RUNE_555
+        )
 
     override fun defineListeners() {
+
         /*
          * Handles lighthouse bookcase.
          */
@@ -52,11 +59,11 @@ class HorrorFromTheDeepPlugin : InteractionListener {
          */
 
         on(Scenery.IRON_LADDER_4383, IntType.SCENERY, "climb") { player, _ ->
-            val questStage = getQuestStage(player, Quests.HORROR_FROM_THE_DEEP)
+            val progress = getVarbit(player, Vars.VARBIT_QUEST_HORROR_FROM_THE_DEEP_PROGRESS_34)
             val teleportLocation =
                 when {
-                    isQuestComplete(player, Quests.HORROR_FROM_THE_DEEP) -> Location(2519, 9994, 1)
-                    questStage >= 40 -> Location(2519, 4618, 1)
+                    progress >= 100 -> Location(2519, 9994, 1)
+                    progress >= 40 -> Location(2519, 4618, 1)
                     else -> null
                 }
 
@@ -78,14 +85,11 @@ class HorrorFromTheDeepPlugin : InteractionListener {
          */
 
         on(Scenery.DOORWAY_4577, IntType.SCENERY, "walk-through") { player, node ->
-            val questStage = getQuestStage(player, Quests.HORROR_FROM_THE_DEEP)
+            val progress = getVarbit(player, Vars.VARBIT_QUEST_HORROR_FROM_THE_DEEP_PROGRESS_34)
 
             when {
-                isQuestComplete(player, Quests.HORROR_FROM_THE_DEEP) -> {
-                    DoorActionHandler.handleAutowalkDoor(player, node.asScenery())
-                }
-
-                questStage >= 20 -> {
+                progress >= 100 -> DoorActionHandler.handleAutowalkDoor(player, node.asScenery())
+                progress >= 20 -> {
                     val insideLighthouse = inBorders(player, 2508, 3634, 2510, 3635)
                     val teleportLocation = if (insideLighthouse) location(2445, 4596, 0) else location(2509, 3635, 0)
 
@@ -95,27 +99,23 @@ class HorrorFromTheDeepPlugin : InteractionListener {
                             override fun pulse(): Boolean {
                                 if (insideLighthouse) {
                                     sendMessage(player, "You unlock the Lighthouse front door.")
-                                    setQuestStage(player, Quests.HORROR_FROM_THE_DEEP, 30)
+                                    setVarbit(player, Vars.VARBIT_HORROR_FROM_THE_DEEP_LIGHTHOUSE_UNLOCKED_39, 1, true)
                                 }
                                 runTask(player, 3) { teleport(player, teleportLocation) }
                                 return true
                             }
-                        },
+                        }
                     )
                     DoorActionHandler.handleAutowalkDoor(player, node.asScenery())
                 }
-
-                questStage < 20 -> {
+                progress < 20 -> {
                     sendNPCDialogue(
                         player,
                         NPCs.LARRISSA_1336,
-                        "Please adventurer... We are both curious as to what has happened in that lighthouse, but you need to fix the bridge for me!",
+                        "Please adventurer... We are both curious as to what has happened in that lighthouse, but you need to fix the bridge for me!"
                     )
                 }
-
-                else -> {
-                    sendDialogue(player, "You can't see any way to open the door.")
-                }
+                else -> sendDialogue(player, "You can't see any way to open the door.")
             }
             return@on true
         }
@@ -125,17 +125,20 @@ class HorrorFromTheDeepPlugin : InteractionListener {
          */
 
         onUseWith(IntType.SCENERY, Items.SWAMP_TAR_1939, Scenery.LIGHTING_MECHANISM_4588) { player, used, with ->
-            val swampTarVarbit = Vars.VARBIT_HORROR_FROM_THE_DEEP_SWAMP_TAR_LIGHTING_MECHANISM_46
-            if (getVarbit(player, swampTarVarbit) == 1) {
-                sendMessage(player, "The torch is already flammable.")
-            } else if (removeItem(player, used.id)) {
+            val varbit = Vars.VARBIT_HORROR_FROM_THE_DEEP_SWAMP_TAR_LIGHTING_MECHANISM_46
+            if (getVarbit(player, varbit) == 1) sendMessage(player, "The torch is already flammable.")
+            else if (removeItem(player, used.id)) {
                 sendMessage(player, "You use the swamp tar to make the torch flammable again.")
-                setVarbit(player, swampTarVarbit, 1, true)
+                setVarbit(player, varbit, 1, true)
             }
             return@onUseWith true
         }
 
-        onUseWith(IntType.SCENERY, Items.TINDERBOX_590, Scenery.LIGHTING_MECHANISM_4588) { player, used, with ->
+        /*
+         * Handles using tinderbox on lighting mechanism.
+         */
+
+        onUseWith(IntType.SCENERY, Items.TINDERBOX_590, Scenery.LIGHTING_MECHANISM_4588) { player, _, _ ->
             val swampTarVarbit = Vars.VARBIT_HORROR_FROM_THE_DEEP_SWAMP_TAR_LIGHTING_MECHANISM_46
             val litVarbit = Vars.VARBIT_HORROR_FROM_THE_DEEP_LIT_LIGHTING_MECHANISM_48
 
@@ -149,6 +152,10 @@ class HorrorFromTheDeepPlugin : InteractionListener {
             return@onUseWith true
         }
 
+        /*
+         * Handles repair lighting mechanism using molten glass.
+         */
+
         onUseWith(IntType.SCENERY, Items.MOLTEN_GLASS_1775, Scenery.LIGHTING_MECHANISM_4588) { player, used, with ->
             val repairedVarbit = Vars.VARBIT_HORROR_FROM_THE_DEEP_LIGHTING_MECHANISM_LENS_REPAIRED_47
             if (getVarbit(player, repairedVarbit) == 1) {
@@ -157,8 +164,8 @@ class HorrorFromTheDeepPlugin : InteractionListener {
             }
             if (removeItem(player, used.id)) {
                 replaceScenery(with.asScenery(), Scenery.LIGHTING_MECHANISM_4587, 80)
-                setQuestStage(player, Quests.HORROR_FROM_THE_DEEP, 40)
                 setVarbit(player, repairedVarbit, 1, true)
+                setVarbit(player, Vars.VARBIT_QUEST_HORROR_FROM_THE_DEEP_PROGRESS_34, 40, true)
                 sendMessage(player, "You use the molten glass to repair the lens.")
                 sendMessage(player, "You have managed to repair the lighthouse torch!")
             }
@@ -171,8 +178,9 @@ class HorrorFromTheDeepPlugin : InteractionListener {
 
         on(strangeWalls, IntType.SCENERY, "study") { player, _ ->
             when (player.location.y) {
-                4626, 10002 -> {
-                    setAttribute(player, GameAttributes.QUEST_HFTD_STRANGE_WALL_DISCOVER, true)
+                4626,
+                10002 -> {
+                    setVarbit(player, Vars.VARBIT_HORROR_FROM_THE_DEEP_STRANGE_WALL_UNLOCKED_35, 1, true)
                     openInterface(player, Components.HORROR_METALDOOR_142)
                 }
                 else -> sendMessage(player, "You cannot see anything unusual about the wall from this side.")
@@ -194,20 +202,22 @@ class HorrorFromTheDeepPlugin : InteractionListener {
          */
 
         on(strangeDoors, IntType.SCENERY, "open") { player, node ->
-            val questStage = getQuestStage(player, Quests.HORROR_FROM_THE_DEEP)
-            if (questStage >= 50) {
+            val progress = getVarbit(player, Vars.VARBIT_QUEST_HORROR_FROM_THE_DEEP_PROGRESS_34)
+            if (progress >= 50) {
                 openDoor(player, node.asScenery())
                 playAudio(player, Sounds.STRANGEDOOR_OPEN_1626)
                 playAudio(player, Sounds.STRANGEDOOR_CLOSE_1625, 2)
-                if (!isQuestComplete(player, Quests.HORROR_FROM_THE_DEEP)) {
-                    setQuestStage(player, Quests.HORROR_FROM_THE_DEEP, 55)
-                    removeAttribute(player, GameAttributes.QUEST_HFTD_STRANGE_WALL_DISCOVER)
+                if (progress < 100) {
+                    setVarbit(player, Vars.VARBIT_QUEST_HORROR_FROM_THE_DEEP_PROGRESS_34, 55, true)
                 }
                 return@on true
             }
+
             when (player.location.y) {
-                4626, 10002 -> sendMessage(player, "You cannot see any way to move this part of the wall....")
-                4627, 10003 -> sendMessage(player, "You cannot see anything unusual about the wall from this side.")
+                4626,
+                10002 -> sendMessage(player, "You cannot see any way to move this part of the wall....")
+                4627,
+                10003 -> sendMessage(player, "You cannot see anything unusual about the wall from this side.")
             }
             return@on true
         }
@@ -217,37 +227,37 @@ class HorrorFromTheDeepPlugin : InteractionListener {
          */
 
         onUseWith(IntType.SCENERY, Items.PLANK_960, *brokenBridge) { player, _, bridge ->
-            val questStage = getQuestStage(player, Quests.HORROR_FROM_THE_DEEP)
-            val bridgeUnlocked = getAttribute(player, GameAttributes.QUEST_HFTD_UNLOCK_BRIDGE, 0)
+            val eastRepaired = getVarbit(player, Vars.VARBIT_HORROR_FROM_THE_DEEP_EAST_BROKEN_BRIDGE_REPAIRED_37)
+            val westRepaired = getVarbit(player, Vars.VARBIT_HORROR_FROM_THE_DEEP_WEST_BROKEN_BRIDGE_REPAIRED_36)
 
             val isFirstHalf = bridge.id == Scenery.BROKEN_BRIDGE_4615
-            val requiredStage = if (isFirstHalf) 5..10 else 10..20
-            val newUnlockValue = if (isFirstHalf) 1 else 2
-            val newQuestStage = if (isFirstHalf) 10 else 20
+            val requiredStage = if (isFirstHalf) 0..10 else 10..20
+            val newUnlockVarbit =
+                if (isFirstHalf) Vars.VARBIT_HORROR_FROM_THE_DEEP_EAST_BROKEN_BRIDGE_REPAIRED_37
+                else Vars.VARBIT_HORROR_FROM_THE_DEEP_WEST_BROKEN_BRIDGE_REPAIRED_36
+            val newProgress = if (isFirstHalf) 10 else 20
             val message =
-                if (isFirstHalf) "You create half a makeshift walkway out of the plank." else "You have now made a makeshift walkway over the bridge."
+                if (isFirstHalf) "You create half a makeshift walkway out of the plank."
+                else "You have now made a makeshift walkway over the bridge."
 
+            val progress = getVarbit(player, Vars.VARBIT_QUEST_HORROR_FROM_THE_DEEP_PROGRESS_34)
             when {
-                questStage !in requiredStage -> {
+                progress !in requiredStage -> {
                     sendDialogue(player, "That won't help fix the bridge.")
                     return@onUseWith false
                 }
-
-                isFirstHalf && bridgeUnlocked == 1 -> {
+                isFirstHalf && eastRepaired == 1 -> {
                     sendDialogue(player, "You have already fixed this half of the bridge.")
                     return@onUseWith false
                 }
-
-                !isFirstHalf && bridgeUnlocked < 1 -> {
+                !isFirstHalf && westRepaired < 1 -> {
                     sendMessage(player, "I might be able to make it to the other side.")
                     return@onUseWith false
                 }
-
                 !inInventory(player, Items.HAMMER_2347, 1) -> {
                     sendDialogue(player, "You need a hammer to force the nails in with.")
                     return@onUseWith false
                 }
-
                 amountInInventory(player, Items.STEEL_NAILS_1539) < 30 -> {
                     sendDialogue(player, "You need 30 steel nails to attach the plank with.")
                     return@onUseWith false
@@ -259,8 +269,8 @@ class HorrorFromTheDeepPlugin : InteractionListener {
                 animate(player, Animations.SMITH_HAMMER_898)
                 queueScript(player, 3, QueueStrength.SOFT) {
                     animate(player, Animations.SMITH_HAMMER_898)
-                    setAttribute(player, GameAttributes.QUEST_HFTD_UNLOCK_BRIDGE, newUnlockValue)
-                    setQuestStage(player, Quests.HORROR_FROM_THE_DEEP, newQuestStage)
+                    setVarbit(player, newUnlockVarbit, 1, true)
+                    setVarbit(player, Vars.VARBIT_QUEST_HORROR_FROM_THE_DEEP_PROGRESS_34, newProgress, true)
                     sendDialogue(player, message)
                     sendMessage(player, message)
                     return@queueScript stopExecuting(player)
@@ -277,25 +287,23 @@ class HorrorFromTheDeepPlugin : InteractionListener {
          */
 
         on(Scenery.BROKEN_BRIDGE_4615, IntType.SCENERY, "cross") { player, _ ->
-            val bridgeUnlock = getAttribute(player, GameAttributes.QUEST_HFTD_UNLOCK_BRIDGE, 0)
-            val cycle = animationCycles(Animations.AGILITY_START_3276)
-            val destination = Location(2598, 3608, 0)
-            if (getAttribute(player, GameAttributes.QUEST_HFTD_UNLOCK_BRIDGE, 0) == 0) {
+            val eastRepaired = getVarbit(player, Vars.VARBIT_HORROR_FROM_THE_DEEP_EAST_BROKEN_BRIDGE_REPAIRED_37)
+            if (eastRepaired == 0) {
                 sendMessage(player, "I might be able to make it to the other side.")
                 return@on true
             }
-            when (bridgeUnlock) {
-                1 -> {
-                    player.animate(Animation(Animations.JUMP_BRIDGE_769), 1)
-                    forceWalk(player, destination.transform(1, 0, 0), "")
-                    teleport(player, destination, TeleportManager.TeleportType.INSTANT, 2)
-                    runTask(player, 3) { forceWalk(player, destination.transform(1, 0, 0), "") }
-                }
-                2 -> {
-                    forceMove(player, player.location, destination, 0, cycle, Direction.EAST, Animations.AGILITY_START_3276)
-                    runTask(player, 6) { forceWalk(player, destination.transform(1, 0, 0), "") }
-                }
-                else -> return@on false
+
+            val cycle = animationCycles(Animations.AGILITY_START_3276)
+            val destination = Location(2598, 3608, 0)
+
+            if (eastRepaired == 1) {
+                player.animate(Animation(Animations.JUMP_BRIDGE_769), 1)
+                forceWalk(player, destination.transform(1, 0, 0), "")
+                teleport(player, destination, TeleportManager.TeleportType.INSTANT, 2)
+                runTask(player, 3) { forceWalk(player, destination.transform(1, 0, 0), "") }
+            } else if (eastRepaired == 2) {
+                forceMove(player, player.location, destination, 0, cycle, Direction.EAST, Animations.AGILITY_START_3276)
+                runTask(player, 6) { forceWalk(player, destination.transform(1, 0, 0), "") }
             }
 
             return@on true
@@ -306,11 +314,20 @@ class HorrorFromTheDeepPlugin : InteractionListener {
          */
 
         on(Scenery.BROKEN_BRIDGE_4616, IntType.SCENERY, "cross") { player, _ ->
-            if (getAttribute(player, GameAttributes.QUEST_HFTD_UNLOCK_BRIDGE, 0) != 2) {
+            val westRepaired = getVarbit(player, Vars.VARBIT_HORROR_FROM_THE_DEEP_WEST_BROKEN_BRIDGE_REPAIRED_36)
+            if (westRepaired != 2) {
                 sendMessage(player, "I might be able to make it to the other side.")
                 return@on true
             }
-            forceMove(player, player.location, Location(2596, 3608, 0), 0, 120, Direction.WEST, Animations.AGILITY_START_ALT_3277)
+            forceMove(
+                player,
+                player.location,
+                Location(2596, 3608, 0),
+                0,
+                120,
+                Direction.WEST,
+                Animations.AGILITY_START_ALT_3277
+            )
             runTask(player, 6) { forceWalk(player, Location.create(2595, 3608, 0), "") }
             return@on true
         }
@@ -322,28 +339,33 @@ class HorrorFromTheDeepPlugin : InteractionListener {
             stage = 0
         }
 
-        private val books = arrayOf(
-            Item(Items.MANUAL_3847, 1),
-            Item(Items.DIARY_3846, 1),
-            Item(Items.JOURNAL_3845, 1)
-        )
+        private val books = arrayOf(Item(Items.MANUAL_3847, 1), Item(Items.DIARY_3846, 1), Item(Items.JOURNAL_3845, 1))
 
         override fun handle(componentID: Int, buttonID: Int) {
             when(stage) {
                 0 -> {
-                    sendDialogue(player!!, "There are three books here that look important... What would you like to do?")
+                    sendDialogue(
+                        player!!,
+                        "There are three books here that look important... What would you like to do?"
+                    )
                     stage = 1
                 }
                 1 -> {
-                    options("Take the Lighthouse Manual", "Take the ancient Diary", "Take Jossik's Journal", "Take all three books")
+                    options(
+                        "Take the Lighthouse Manual",
+                        "Take the ancient Diary",
+                        "Take Jossik's Journal",
+                        "Take all three books"
+                    )
                     stage = 2
                 }
                 2 -> {
-                    val bookIDs = books.toList()
                     val freeSlots = freeSlots(player!!)
-
-                    if (buttonID == 4 && freeSlots < bookIDs.size || buttonID in 1..3 && freeSlots < 1) {
-                        sendDialogue(player!!, "You do not have enough room to take ${if (buttonID == 4) "all three" else "that"}.")
+                    if (buttonID == 4 && freeSlots < books.size || buttonID in 1..3 && freeSlots < 1) {
+                        sendDialogue(
+                            player!!,
+                            "You do not have enough room to take ${if (buttonID == 4) "all three" else "that"}."
+                        )
                         return
                     }
 
--- Server/src/main/content/region/fremennik/lighthouse/quest/horror/plugin/HorrorMetalDoorInterface.kt
+++ Server/src/main/content/region/fremennik/lighthouse/quest/horror/plugin/HorrorMetalDoorInterface.kt
@@ -1,6 +1,5 @@
 package content.region.fremennik.lighthouse.quest.horror.plugin
 
-import content.data.GameAttributes
 import core.api.*
 import core.api.isQuestComplete
 import core.api.setQuestStage
@@ -9,32 +8,47 @@ import core.game.interaction.QueueStrength
 import shared.consts.Components
 import shared.consts.Quests
 import shared.consts.Sounds
+import shared.consts.Vars
 
 class HorrorMetalDoorInterface : InterfaceListener {
+
     override fun defineInterfaceListeners() {
         onOpen(Components.HORROR_METALDOOR_142) { player, _ ->
             val questComplete = isQuestComplete(player, Quests.HORROR_FROM_THE_DEEP)
-            val runeAttributes =
-                listOf(
-                    GameAttributes.QUEST_HFTD_USE_FIRE_RUNE to 2,
-                    GameAttributes.QUEST_HFTD_USE_AIR_RUNE to 3,
-                    GameAttributes.QUEST_HFTD_USE_EARTH_RUNE to 4,
-                    GameAttributes.QUEST_HFTD_USE_WATER_RUNE to 5,
-                    GameAttributes.QUEST_HFTD_USE_ARROW to 6,
-                    GameAttributes.QUEST_HFTD_USE_SWORD to 7,
-                )
 
-            runeAttributes.forEach { (attribute, componentIndex) ->
+            /*
+             * Map runes/items to component indices
+             */
+
+            val vbit = listOf(
+                Vars.VARBIT_HORROR_FROM_THE_DEEP_STRANGE_WALL_FIRE_RUNE_40 to 2,
+                Vars.VARBIT_HORROR_FROM_THE_DEEP_STRANGE_WALL_AIR_RUNE_43 to 3,
+                Vars.VARBIT_HORROR_FROM_THE_DEEP_STRANGE_WALL_EARTH_RUNE_42 to 4,
+                Vars.VARBIT_HORROR_FROM_THE_DEEP_STRANGE_WALL_WATER_RUNE_41 to 5,
+                Vars.VARBIT_HORROR_FROM_THE_DEEP_STRANGE_WALL_ARROW_45 to 6,
+                Vars.VARBIT_HORROR_FROM_THE_DEEP_STRANGE_WALL_SWORD_44 to 7
+            )
+
+            /*
+             * Set visibility of components based on varbit state
+             */
+
+            vbit.forEach { (varbit, componentIndex) ->
                 setComponentVisibility(
                     player,
                     Components.HORROR_METALDOOR_142,
                     componentIndex,
-                    getAttribute(player, attribute, 0) != 1,
+                    getVarbit(player, varbit) == 0
                 )
             }
+
             if (questComplete) return@onOpen true
 
-            if (getAttribute(player, GameAttributes.QUEST_HFTD_UNLOCK_DOOR, 0) > 5) {
+            /*
+             * Unlock door if varbit indicates progress
+             */
+
+            if (getVarbit(player, Vars.VARBIT_HORROR_FROM_THE_DEEP_STRANGE_WALL_UNLOCKED_35) > 0) {
                 closeInterface(player)
                 queueScript(player, 1, QueueStrength.SOFT) {
                     sendMessage(player, "You hear the sound of something moving within the wall.")
