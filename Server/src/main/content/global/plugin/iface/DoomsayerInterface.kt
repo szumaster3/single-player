a0b116d54e5d07e04f2a1532eaf2c2e370f3920a
szumaster3 <szumaster2009@protonmail.com>
2025-10-30 09:35:57 +0100

    Add Icy path warning, split god book preaching text, misc

--- Server/src/main/content/global/plugin/iface/DoomsayerInterface.kt
+++ Server/src/main/content/global/plugin/iface/DoomsayerInterface.kt
@@ -13,29 +13,33 @@ import shared.consts.Components
  * @author szu
  */
 class DoomsayerInterface : InterfaceListener {
+
     override fun defineInterfaceListeners() {
         /*
          * Handles toggle of warnings.
          */
 
         on(Components.CWS_DOOMSAYER_583) { player, _, _, buttonID, _, _ ->
-            val warning = Warnings.values().find { it.buttonId == buttonID }
-
+            val warning = Warnings.values().find { it.buttonId == buttonID } ?: return@on true
             if (buttonID == 81) {
-                sendMessage(player, "This option is only available on PVP worlds.")
+                sendMessage(player, "This option is only available on PvP worlds.")
                 return@on true
             }
 
-            if (warning != null) {
-                if (getVarbit(player, warning.varbit) < 6) {
-                    sendMessages(player, "You cannot toggle the warning screen on or off. You need to go to the area it", "is linked to enough times to have the option to do so.")
-                    return@on true
-                }
-
-                val isDisbled = WarningManager.isWarningDisabled(player, warning)
-                WarningManager.toggleWarning(player, warning.component)
-                sendMessage(player, "You have ${if (!isDisbled) "disabled" else "enabled"} the warning.")
+            val timesSeen = getVarbit(player, warning.varbit)
+            if (timesSeen < 6) {
+                sendMessages(
+                    player,
+                    "You cannot toggle this warning screen on or off. You need to go to the area it is linked to enough times to have the option to do so."
+                )
+                return@on true
             }
+
+            val wasDisabled = WarningManager.isWarningDisabled(player, warning)
+            WarningManager.toggleWarning(player, warning)
+
+            val status = if (wasDisabled) "enabled" else "disabled"
+            sendMessage(player, "You have $status the ${warning.name.lowercase().replace('_', ' ')} warning.")
             return@on true
         }
     }
