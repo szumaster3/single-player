a438c5d65687c94a4b56324cb863a0152372e722
szumaster3 <szumaster2009@protonmail.com>
2025-11-15 00:29:07 +0100

    Made the all doors openable at poh

--- Server/src/main/content/global/skill/construction/decoration/DoorPlugin.kt
+++ Server/src/main/content/global/skill/construction/decoration/DoorPlugin.kt
@@ -8,23 +8,20 @@ import core.game.interaction.OptionHandler
 import core.game.node.Node
 import core.game.node.entity.player.Player
 import core.game.node.scenery.Scenery
+import core.game.node.scenery.SceneryBuilder
 import core.plugin.Initializable
 import core.plugin.Plugin
+import core.game.world.map.Location
 
 @Initializable
 class DoorPlugin : OptionHandler() {
-    @Throws(Throwable::class)
+
     override fun newInstance(arg: Any?): Plugin<Any> {
         for (style in HousingStyle.values()) {
             SceneryDefinition.forId(style.doorId).handlers["option:open"] = this
             SceneryDefinition.forId(style.secondDoorId).handlers["option:open"] = this
         }
-        for (deco in BuildHotspot.DUNGEON_DOOR_LEFT.decorations) {
-            SceneryDefinition.forId(deco.objectId).handlers["option:open"] = this
-            SceneryDefinition.forId(deco.objectId).handlers["option:pick-lock"] = this
-            SceneryDefinition.forId(deco.objectId).handlers["option:force"] = this
-        }
-        for (deco in BuildHotspot.DUNGEON_DOOR_RIGHT.decorations) {
+        for (deco in BuildHotspot.DUNGEON_DOOR_LEFT.decorations + BuildHotspot.DUNGEON_DOOR_RIGHT.decorations) {
             SceneryDefinition.forId(deco.objectId).handlers["option:open"] = this
             SceneryDefinition.forId(deco.objectId).handlers["option:pick-lock"] = this
             SceneryDefinition.forId(deco.objectId).handlers["option:force"] = this
@@ -33,46 +30,121 @@ class DoorPlugin : OptionHandler() {
     }
 
     override fun handle(player: Player, node: Node, option: String): Boolean {
-        val door = node as Scenery
-        val second = DoorActionHandler.getSecondDoor(door) ?: return true
+        val door = node as? Scenery ?: return true
 
-        when (option) {
-            "open" -> DoorActionHandler.open(
-                door, second,
-                getReplaceId(door), getReplaceId(second),
-                true, -1,  false
-            )
+        try {
+            val x = door.location.chunkOffsetX
+            val y = door.location.chunkOffsetY
+
+            val secondDoor = when {
+                y == 0 || y == 7 -> when (x) {
+                    3 -> Scenery(door.id, Location.create(door.location.x + 1, door.location.y, door.location.z), door.type, door.rotation)
+                    4 -> Scenery(door.id, Location.create(door.location.x - 1, door.location.y, door.location.z), door.type, door.rotation)
+                    else -> null
+                }
+                x == 0 || x == 7 -> when (y) {
+                    3 -> Scenery(door.id, Location.create(door.location.x, door.location.y + 1, door.location.z), door.type, door.rotation)
+                    4 -> Scenery(door.id, Location.create(door.location.x, door.location.y - 1, door.location.z), door.type, door.rotation)
+                    else -> null
+                }
+                else -> null
+            }
+            if (secondDoor != null) {
+                DoorActionHandler.open(door, secondDoor, door.id + 2, secondDoor.id + 2,true,-1, false)
+            }
+            handleDoor(door, secondDoor)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
 
         return true
     }
 
+    private fun handleDoor(door: Scenery, second: Scenery?) {
+        val replaceId = getReplaceId(door)
+        val secondReplaceId = second?.let { getReplaceId(it) } ?: -1
 
-    private fun getReplaceId(`object`: Scenery): Int {
-        for (data in REPLACEMENT) {
-            if (`object`.id == data[0]) {
-                return data[1]
-            }
+        if (door.id in CLOSED_IDS) {
+            openDoor(door, second, replaceId, secondReplaceId)
+        } else {
+            closeDoor(door, second, replaceId, secondReplaceId)
         }
-        return `object`.id + 6
     }
 
+    private fun openDoor(door: Scenery, second: Scenery?, replaceId: Int, secondReplaceId: Int) {
+        val (offsetX, offsetY, newRot) = getOpenOffset(door)
+        val newDoor = Scenery(replaceId, door.location.transform(offsetX, offsetY, 0), door.type, newRot)
+        SceneryBuilder.replace(door, newDoor)
 
+        second?.let {
+            val (sOffsetX, sOffsetY, sRot) = getOpenOffset(it)
+            val newSecond = Scenery(secondReplaceId, it.location.transform(sOffsetX, sOffsetY, 0), it.type, sRot)
+            SceneryBuilder.replace(it, newSecond)
+        }
+    }
+
+    private fun closeDoor(door: Scenery, second: Scenery?, replaceId: Int, secondReplaceId: Int) {
+        val (offsetX, offsetY, newRot) = getCloseOffset(door)
+        val newDoor = Scenery(replaceId - 2, door.location.transform(offsetX, offsetY, 0), door.type, newRot)
+        SceneryBuilder.replace(door, newDoor)
+
+        second?.let {
+            val (sOffsetX, sOffsetY, sRot) = getCloseOffset(it)
+            val newSecond = Scenery(secondReplaceId - 2, it.location.transform(sOffsetX, sOffsetY, 0), it.type, sRot)
+            SceneryBuilder.replace(it, newSecond)
+        }
+    }
+
+    private fun getOpenOffset(door: Scenery): Triple<Int, Int, Int> {
+        return when {
+            door.location.chunkOffsetY == 0 || door.location.chunkOffsetY == 7 -> {
+                val offsetY = if (door.location.chunkOffsetY == 7) 1 else -1
+                val rot = if (door.location.chunkOffsetX == 3) 0 else 2
+                Triple(0, offsetY, rot)
+            }
+            door.location.chunkOffsetX == 0 || door.location.chunkOffsetX == 7 -> {
+                val offsetX = if (door.location.chunkOffsetX == 7) 1 else -1
+                val rot = if (door.location.chunkOffsetY == 3) 3 else 1
+                Triple(offsetX, 0, rot)
+            }
+            else -> Triple(0, 0, door.rotation)
+        }
+    }
+
+    private fun getCloseOffset(door: Scenery): Triple<Int, Int, Int> {
+        return when {
+            door.location.chunkOffsetY == 0 || door.location.chunkOffsetY == 7 -> {
+                val offsetY = if (door.location.chunkOffsetY == 7) 1 else -1
+                val rot = if (door.location.chunkOffsetY == 7) 3 else 1
+                Triple(0, offsetY, rot)
+            }
+            door.location.chunkOffsetX == 0 || door.location.chunkOffsetX == 7 -> {
+                val offsetX = if (door.location.chunkOffsetX == 7) 1 else -1
+                val rot = if (door.location.chunkOffsetX == 7) 0 else 2
+                Triple(offsetX, 0, rot)
+            }
+            else -> Triple(0, 0, door.rotation)
+        }
+    }
+
+    private fun getReplaceId(door: Scenery): Int = REPLACEMENT_MAP[door.id] ?: (door.id + 2)
+    private val CLOSED_IDS = REPLACEMENT_MAP.keys
 
     companion object {
-        val REPLACEMENT = arrayOf(
-            intArrayOf(13100, 13102),
-            intArrayOf(13101, 13103),
-            intArrayOf(13006, 13008),
-            intArrayOf(13007, 13008),
-            intArrayOf(13015, 13017),
-            intArrayOf(13016, 13018),
-            intArrayOf(13094, 13095),
-            intArrayOf(13096, 13097),
-            intArrayOf(13109, 13110),
-            intArrayOf(13107, 13108),
-            intArrayOf(13118, 13120),
-            intArrayOf(13119, 13121)
+        private val REPLACEMENT_MAP = mapOf(
+            13100 to 13102,
+            13101 to 13103,
+            13006 to 13008,
+            13007 to 13008,
+            13015 to 13017,
+            13016 to 13018,
+            13094 to 13095,
+            13096 to 13097,
+            13109 to 13110,
+            13107 to 13108,
+            13118 to 13120,
+            13119 to 13121
         )
     }
 }
