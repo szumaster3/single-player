c28aa2376d58b4466149e68d8236c004853af0b0
szumaster3 <szumaster2009@protonmail.com>
2025-11-14 06:31:03 +0100

    Update salarin twisted npc

--- Server/src/main/content/region/kandarin/yanille/npc/SalarinTwistedNPC.kt
+++ Server/src/main/content/region/kandarin/yanille/npc/SalarinTwistedNPC.kt
@@ -5,37 +5,44 @@ import core.game.node.entity.combat.CombatStyle
 import core.game.node.entity.npc.AbstractNPC
 import core.game.world.map.Location
 import core.plugin.Initializable
+import core.tools.RandomFunction
 import shared.consts.NPCs
 
+/**
+ * Represents the Salarin Twisted npc.
+ */
 @Initializable
 class SalarinTwistedNPC : AbstractNPC {
-    val SPELL_IDS = intArrayOf(1, 4, 6, 8)
+
+    companion object {
+        private val SPELL_IDS = setOf(1, 4, 6, 8)
+    }
 
     constructor() : super(-1, null)
 
     constructor(id: Int, location: Location) : super(id, location) {
-        super.setAggressive(true)
+        isAggressive = true
+        isWalks = true
+        isNeverWalks = false
     }
 
-    override fun construct(id: Int, location: Location, vararg objects: Any?): AbstractNPC = SalarinTwistedNPC(id, location)
+    override fun construct(id: Int, location: Location, vararg objects: Any?): AbstractNPC =
+        SalarinTwistedNPC(id, location)
 
     override fun checkImpact(state: BattleState) {
-        if (state.style != CombatStyle.MAGIC) {
-            state.neutralizeHits()
-            return
-        }
-        if (state.spell == null) {
-            state.neutralizeHits()
-            return
-        }
-        val spell = state.spell
-        for (id in SPELL_IDS) {
-            if (id == spell.spellId) {
-                state.estimatedHit = state.maximumHit
-                return
+        if (state.style != CombatStyle.MAGIC || state.spell == null) {
+            if (RandomFunction.random(100) < 50) {
+                sendChat("Your pitiful attacks cannot hurt me!")
             }
+            state.neutralizeHits()
+            return
         }
-        state.neutralizeHits()
+
+        if (state.spell.spellId in SPELL_IDS) {
+            state.estimatedHit = state.maximumHit
+        } else {
+            state.neutralizeHits()
+        }
     }
 
     override fun getIds(): IntArray = intArrayOf(NPCs.SALARIN_THE_TWISTED_205)
