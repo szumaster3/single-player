c1c8cfad3c9ae462653e3f29d1b44a7b79dd37a1
szumaster3 <szumaster2009@protonmail.com>
2025-11-17 07:55:00 +0100

    Remove problem with poh chairs XD

--- Server/src/main/content/global/skill/construction/decoration/ChairPlugin.kt
+++ Server/src/main/content/global/skill/construction/decoration/ChairPlugin.kt
@@ -1,12 +1,14 @@
 package content.global.skill.construction.decoration
 
-import core.api.forceMove
+import core.api.*
 import core.cache.def.impl.SceneryDefinition
 import core.game.interaction.OptionHandler
 import core.game.node.Node
 import core.game.node.entity.player.Player
 import core.game.node.scenery.Scenery
+import core.game.node.scenery.SceneryType
 import core.game.system.task.Pulse
+import core.game.world.map.Direction
 import core.game.world.update.flag.context.Animation
 import core.plugin.Initializable
 import core.plugin.Plugin
@@ -28,23 +30,31 @@ class ChairPlugin : OptionHandler() {
         val scenery = node as? Scenery ?: return false
         val chair = Chairs.fromId(scenery.id) ?: return false
 
-        val animID = if (scenery.type == 11) chair.anim + 1 else chair.anim
+        val animID = if (scenery.type == SceneryType.CentrepieceDiagonal) chair.anim + 1 else chair.anim
         val sitAnimID = chair.sitAnim
 
-        forceMove(player, player.location, scenery.location, 0, 30, node.direction.opposite, sitAnimID)
+        // Always north, the rest is handled by animation.
+        forceMove(player, player.location, scenery.location, 0, 30, Direction.NORTH, sitAnimID)
 
         player.locks.lockInteractions(600_000)
+
+        registerLogoutListener(player, "someone else house") {
+            replaceScenery(Scenery(if(scenery.id != 83) node.id else 83,node.location,node.rotation,Direction.SOUTH),node.id,-1)
+        }
 
         player.pulseManager.run(object : Pulse(2) {
             override fun pulse(): Boolean {
                 player.animate(Animation.create(animID))
+                replaceScenery(node.asScenery(),83,-1)
                 return false
             }
 
             override fun stop() {
                 super.stop()
+                clearLogoutListener(player, "someone else house")
                 player.locks.unlockInteraction()
-                player.animate(Animation.create(sitAnimID + 2))
+                replaceScenery(Scenery(83,node.location,node.rotation,Direction.SOUTH),node.id,-1)
+                forceMove(player,player.location,player.location.transform(node.direction.opposite, 1),0,30,null,sitAnimID+2)
             }
         })
 
