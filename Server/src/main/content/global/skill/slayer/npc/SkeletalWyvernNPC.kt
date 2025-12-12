4c94a1117c249e0fd8094d220556787f823a9a2b
szumaster3 <szumaster2009@protonmail.com>
2025-12-12 14:20:19 +0100

    CtrlAltShiftK the SkillingResource & Tasks

--- Server/src/main/content/global/skill/slayer/npc/SkeletalWyvernNPC.kt
+++ Server/src/main/content/global/skill/slayer/npc/SkeletalWyvernNPC.kt
@@ -14,39 +14,15 @@ import core.game.world.update.flag.context.Graphics
 import shared.consts.Animations
 import shared.consts.Items
 
-class SkeletalWyvernNPC : NPCBehavior(*Tasks.SKELETAL_WYVERN.ids) {
+class SkeletalWyvernNPC : NPCBehavior(*Tasks.SKELETAL_WYVERN.npcs) {
 
-    private val COMBAT_HANDLER = MultiSwingHandler(SwitchAttack(
-        CombatStyle.MELEE.swingHandler,
-        Animation(Animations.STAND_2985)
-    ),
-        SwitchAttack(
-            CombatStyle.RANGE.swingHandler,
-            Animation(2989),
-            Graphics(499)
-        ),
-        DragonfireSwingHandler.get(
-            false,
-            54,
-            Animation(2988),
-            Graphics(501),
-            null,
-            null,
-            false
-        )
+    private val COMBAT_HANDLER = MultiSwingHandler(SwitchAttack(CombatStyle.MELEE.swingHandler, Animation(Animations.STAND_2985)),
+        SwitchAttack(CombatStyle.RANGE.swingHandler, Animation(2989), Graphics(499)),
+        DragonfireSwingHandler.get(false, 54, Animation(2988), Graphics(501), null, null, false)
     )
-    private val COMBAT_HANDLER_FAR = MultiSwingHandler(SwitchAttack(
-        CombatStyle.RANGE.swingHandler,
-        Animation(2989),
-        Graphics(499)
-    ))
+    private val COMBAT_HANDLER_FAR = MultiSwingHandler(SwitchAttack(CombatStyle.RANGE.swingHandler, Animation(2989), Graphics(499)))
 
-    private val SHIELDS = intArrayOf(
-        Items.DRAGONFIRE_SHIELD_11283,
-        Items.DRAGONFIRE_SHIELD_11285,
-        Items.ELEMENTAL_SHIELD_2890,
-        Items.MIND_SHIELD_9731
-    )
+    private val SHIELDS = intArrayOf(Items.DRAGONFIRE_SHIELD_11283, Items.DRAGONFIRE_SHIELD_11285, Items.ELEMENTAL_SHIELD_2890, Items.MIND_SHIELD_9731)
 
     override fun getSwingHandlerOverride(self: NPC, original: CombatSwingHandler): CombatSwingHandler {
         val victim = self.properties.combatPulse.victim ?: return original
