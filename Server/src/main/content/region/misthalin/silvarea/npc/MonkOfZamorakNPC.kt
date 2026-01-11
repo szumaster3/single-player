bc2ff640725276a15eb9ca8b767270d793fc27b5
szumaster3 <szumaster2009@protonmail.com>
2026-01-11 13:13:58 +0100

    Fix the golden key drop and the npc spell cast height

--- Server/src/main/content/region/misthalin/silvarea/npc/MonkOfZamorakNPC.kt
+++ Server/src/main/content/region/misthalin/silvarea/npc/MonkOfZamorakNPC.kt
@@ -1,32 +1,38 @@
 package content.region.misthalin.silvarea.npc
 
 import core.game.node.entity.Entity
-import core.game.node.entity.npc.AbstractNPC
+import core.game.node.entity.npc.NPC
+import core.game.node.entity.npc.NPCBehavior
 import core.game.node.entity.player.Player
 import core.game.node.item.GroundItemManager
 import core.game.node.item.Item
-import core.game.world.map.Location
-import core.plugin.Initializable
 import shared.consts.Items
 import shared.consts.NPCs
 import shared.consts.Quests
 
-@Initializable
-class MonkOfZamorakNPC(id: Int = 0, location: Location? = null) : AbstractNPC(id, location) {
+class MonkOfZamorakNPC : NPCBehavior(*MONK_NPCS) {
 
-    override fun construct(id: Int, location: Location, vararg objects: Any): AbstractNPC = MonkOfZamorakNPC(id, location)
-
-    override fun finalizeDeath(killer: Entity?) {
-        if (killer is Player) {
-            val player = killer.asPlayer()
-            val quest = player.getQuestRepository().getQuest(Quests.PRIEST_IN_PERIL)
-            if (quest.isStarted(player) && player.viewport.region!!.regionId == 13662) {
-                GroundItemManager.create(Item(Items.GOLDEN_KEY_2944, 1), getLocation(), player)
-            }
-        }
-        super.finalizeDeath(killer)
+    companion object {
+        private val MONK_NPCS = intArrayOf(
+            NPCs.MONK_OF_ZAMORAK_1044,
+            NPCs.MONK_OF_ZAMORAK_1045,
+            NPCs.MONK_OF_ZAMORAK_1046
+        )
     }
 
-    override fun getIds(): IntArray =
-        intArrayOf(NPCs.MONK_OF_ZAMORAK_1046, NPCs.MONK_OF_ZAMORAK_1045, NPCs.MONK_OF_ZAMORAK_1044)
+    override fun onDropTableRolled(
+        self: NPC,
+        killer: Entity,
+        drops: ArrayList<Item>,
+    ) {
+        super.onDropTableRolled(self, killer, drops)
+        if (killer is Player) {
+            val player = killer
+            val quest = player.getQuestRepository().getQuest(Quests.PRIEST_IN_PERIL)
+
+            if (quest.isStarted(player)) {
+                GroundItemManager.create(Item(Items.GOLDEN_KEY_2944, 1), self.location, player)
+            }
+        }
+    }
 }
